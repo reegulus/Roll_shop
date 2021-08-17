fd26fd79f907d5c6bf76dc9c1c3720f63b1dbb3c
Egorych <mikhalevichegor@gmail.com>
2021-08-17 13:27:49 +0300

    Changing the counter in products at the model level

--- js/products/model.js
+++ js/products/model.js
@@ -6,11 +6,26 @@ export default class Model {
     async loadProducts() {
         const response = await fetch('./js/products.json')
         this.products = await response.json()
+
+        for (const product of this.products) {
+            product.counter = 1
+        }
     }
 
     updateCounter(id, action) {
-       const product =  this.products.find((item) => item.id === id)
+
+
+
+        const product = this.products.find((item) => item.id === id)
         console.log(product)
-        console.log(action)
+
+        if (action === 'plus') {
+            ++product.counter
+        }
+
+        if (action === 'minus') {
+            --product.counter
+        }
+        console.log(product)
     }
 }
