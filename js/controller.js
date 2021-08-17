82d4572a3156b291a791be0498f58a6f5e0d3db3
Egorych <mikhalevichegor@gmail.com>
2021-08-17 12:54:52 +0300

    Click on the counter in products and start the method for working with the counter

--- js/controller.js
+++ js/controller.js
@@ -4,13 +4,27 @@ import * as productsView from './products/view.js'
 const productsModel = new ProductsModel()
 
 
-
 //Фсинхронная функция getAndRenderProducts
 // 1. Сначало получение товаров из JSON files
 // 2. Только после этого - отображение товаров на странице
 async function getAndRenderProducts() {
-    await  productsModel.loadProducts()
+    await productsModel.loadProducts()
     productsView.renderProducts(productsModel.products)
 }
 
 getAndRenderProducts()
+
+productsView.elements.productsContainer.addEventListener('click', function (event) {
+    //Совершаемое действие
+    let action = event.target.dataset.action
+
+//Если кликнули по счетчику внутри товаров
+    if(action === "plus" || action === "minus") {
+        //Находим ID продукта
+        const productId = +event.target.closest('.card').dataset.id
+
+        //Запускаем модель для изменения счётчика
+        productsModel.updateCounter(productId, action)
+    }
+
+})
--- js/products/model.js
+++ js/products/model.js
@@ -7,4 +7,10 @@ export default class Model {
         const response = await fetch('./js/products.json')
         this.products = await response.json()
     }
+
+    updateCounter(id, action) {
+       const product =  this.products.find((item) => item.id === id)
+        console.log(product)
+        console.log(action)
+    }
 }
